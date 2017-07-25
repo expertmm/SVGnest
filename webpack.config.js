760270a8dc05a33e2b2f492e1f3efd3cf069ef22
John Rees <john@bitsushi.com>
2017-07-25 23:09:55 +0100

    deploy task (GitHub pages)

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, './js'),
+    path: path.resolve(__dirname, './build/js'),
     publicPath: 'js'
   },
   devServer: {
